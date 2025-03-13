8c17ecf9a8c3eb5f8c9bba7e0b8d7e444544a94d
Julian Sarmiento <juliansarmiento10@outlook.com>
2025-03-12 19:05:42 -0500

    Create DTOs

---
+++ src/Application/TT.WireTransfers.Application/DTOs/TransferDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TT.WireTransfers.Application.DTOs
+{
+    public class TransferDto
+    {
+        public int SourceWalletId { get; set; }
+        public int DestinationWalletId { get; set; }
+        public decimal Amount { get; set; }
+        public int Type { get; set; }
+    }
+}
---
+++ src/Application/TT.WireTransfers.Application/DTOs/WalletDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TT.WireTransfers.Application.DTOs
+{
+    public class WalletDto
+    {
+        public string? DocumentId { get; set; }
+        public string? Name { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
