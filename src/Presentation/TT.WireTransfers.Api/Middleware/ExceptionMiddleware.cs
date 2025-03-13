5d7ae365ba778b2d7ea332eaed94e16d6d4239b6
Julian Sarmiento <juliansarmiento10@outlook.com>
2025-03-12 19:08:41 -0500

    Configure csproj

---
+++ src/Presentation/TT.WireTransfers.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using TT.WireTransfers.Domain.Exceptions;
+
+namespace TT.WireTransfers.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (WalletNotFoundException ex)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex); // Para debug
+                context.Response.StatusCode = 500;
+                var errorResponse = new Dictionary<string, string> { { "error", "Internal server error" } };
+                await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
